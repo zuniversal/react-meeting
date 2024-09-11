52331440ad686b13465b53f1d71de92fbb781ba4
zhuangyubin <604688489@qq.com>
2024-09-11 10:30:32 +0800

    fix: 修改文案

--- src/pages/common/ContactUs/config.js
+++ src/pages/common/ContactUs/config.js
@@ -2,8 +2,8 @@ export const infoConfig = [
   // 'China Ship Scientific Research Center',
   'Adress：Shanshuidong road No.222 Binhu area, Wuxi, Jiangsu, China',
   'Postal code: 214082',
-  'Contact number: 86-0510-85555311',
-  'Fax: 86-0510-85555311',
+  // 'Contact number: 86-0510-85555311',
+  'Fax: 0510-85555193',
   'Email: STABS2024@cssrc.com.cn',
-  'Wechat :STABS2024',
+  // 'Wechat :STABS2024',
 ];
